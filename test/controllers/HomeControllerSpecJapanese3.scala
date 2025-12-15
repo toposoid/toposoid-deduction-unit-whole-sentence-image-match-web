1a6bb6d64964d26cdd8abb59cb397a8495903508
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2025-12-15 11:07:53 +0000

    https://github.com/toposoid/toposoid-deduction-unit-whole-sentence-image-match-web/issues/37

--- test/controllers/HomeControllerSpecJapanese3.scala
+++ test/controllers/HomeControllerSpecJapanese3.scala
@@ -17,7 +17,7 @@
 
 package controllers
 
-import akka.util.Timeout
+import org.apache.pekko.util.Timeout
 import com.ideal.linked.common.DeploymentConverter.conf
 import com.ideal.linked.toposoid.common.{CLAIM, PREMISE, TRANSVERSAL_STATE, ToposoidUtils, TransversalState}
 import com.ideal.linked.toposoid.knowledgebase.regist.model.{PropositionRelation, Reference}
